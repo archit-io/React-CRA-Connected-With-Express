5c9bd50ec4aff1d82915dca567206d770a23bce0
Archit Choudhary <archit.digisocial@gmail.com>
2022-08-15 00:34:28 +0530

    In client create a user component and render it in App.js

---
+++ client/src/components/users.js
@@ -0,0 +1,15 @@
+import { Component } from 'react';
+
+class Users extends Component {
+
+    render() {
+        return (
+            <div>
+                <h1>Users Component</h1>
+            </div>
+        )
+    }
+
+}
+
+export default Users;
